e633f723f67aaa5bc234bb8d3fedae626ee6a43f
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-04-04 22:38:03 +0500

    GalaxyAOSP Changes

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,9 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/crespo.mk \
-    $(LOCAL_DIR)/crespo4g.mk \
-    $(LOCAL_DIR)/emulator.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/stingray.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/wingray.mk
-
+    $(LOCAL_DIR)/
