5bc74b814be12b68e8fc537ff877644c8e47c649
Faryaab Sheikh <faryaabsheikh@gmail.com>
2012-04-10 19:21:01 +0100

    Add ro.modversion line for F1

--- products/common_versions.mk
+++ products/common_versions.mk
@@ -2,7 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_ID=IMM76D BUILD_VERSION_TAGS=release-keys BUILD_UTC_DATE=$(shell date +"%s")
 
 # Rom Manager properties
-#PRODUCT_PROPERTY_OVERRIDES += \
- #   ro.modversion=GalaxyAOSP-$(shell date +"%m-%d-%y") \
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=F1 GalaxyAOSP-$(shell date +"%m-%d-%y") 
   #  ro.rommanager.developerid=Faryaab
 
